3e55b212b47f4b8b809f94a3250efc33e4258f94
sihwan0Lee <stay136@gmail.com>
2020-05-15 17:03:26 +0900

    위스타 그램 입니다

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import SignUpView, SignInView
 
 urlpatterns = [
-    path('/up', SignUpView.as_view()),
-    path('/in', SignInView.as_view()),
+    path('up', SignUpView.as_view()),
+    path('in', SignInView.as_view()),
 ] 
